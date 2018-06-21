bbead6b136886e26455f30ddedf8d8f0a2a1e120
macph <macph@users.noreply.github.com>
2018-06-21 17:13:12 +0100

    Converted text fields and simplified form submission
    Text fields are now search fields in WTForms, so they should show up
    properly on mobile. Selecting filter options will submit the form with
    JS automatically, rather than constructing a new query string.

--- nextbus/forms.py
+++ nextbus/forms.py
@@ -2,11 +2,12 @@
 Forms for searching bus stops.
 """
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms.fields import SubmitField
+from wtforms.fields.html5 import SearchField
 from wtforms.validators import InputRequired
 
 
 class SearchPlaces(FlaskForm):
     """ Full text search for places, stops and postcodes. """
-    search_query = StringField("search", validators=[InputRequired()])
+    search_query = SearchField("search", validators=[InputRequired()])
     submit_query = SubmitField("Search")

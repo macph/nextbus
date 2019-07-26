cbde48ec89749c7b2b12e8588c24cecc544753f4
macph <macph@users.noreply.github.com>
2019-07-26 20:11:37 +0100

    Merged migrations and updated version to 0.8

--- README.md
+++ README.md
@@ -6,47 +6,50 @@ This is a Flask + SQLAlchemy web application running on a PostgreSQL DB.
 
 ## Installation
 
-The easiest way to do this is to use `pipenv`. Clone the repo:
+Clone the repo:
 
-```
+```bash
 git clone https://github.com/macph/nextbus.git
 ```
 
-Enter the new folder and run
+The server can be set up using Docker or installed locally with pipenv. If you're using Docker Compose, `docker_compose.yml` assumes you've added `nxb_config.py` and `gu_config.py` to the `instance` folder ignored by git.
 
-```
-pipenv install
+```bash
+mkdir instance
+
+echo 'SQLALCHEMY_DATABASE_URI = "postgresql://prod:hello_world@postgres:5432/nextbus"
+SECRET_KEY = b"your secret key"' > instance/nxb_config.py
+
+echo 'bind = ["0.0.0.0:8000"]' > instance/gu_config.py
 ```
 
-to setup the virtual environment, install all related package and the application itself. The virtual environment can be started up with `pipenv shell` or a command run with `pipenv run`. The `nxb` command is available for accessing the application.
+See `default_config.py` for all options.
+
+Use `docker-compose up` or `pipenv install` to set up the application.
 
 ## Starting up
 
-PostgreSQL must be at least v11 to support particular full text search functionality.
+PostgreSQL must be at least version 11 to support particular full text search functionality.
 
-Default config values can be found within `default_config.py` in the root directory. If new values are needed, like a different URI for the DB, a separate config file is required. Create a new Python config file in the `instance` folder:
+With the configuration set up, use `docker exec` or `pipenv run` to access the application. Run the database migrations:
 
-```
-echo 'SQLALCHEMY_DATABASE_URI = "postgresql://scott:tiger@localhost"' > instance/new_config.py
-```
-
-and set enviroment variable `APP_CONFIG` to `new_config.py` so the application will load config on start up. With the configuration set up, run the database migrations:
-
-```
+```bash
 nxb db upgrade
 ```
 
 Populate with data:
 
-```
+```bash
 nxb populate --all
 ```
 
-which will download NPTG, NaPTAN and NSPL data and commits them, as well as doing some modifications. If you've added TNDS FTP credentials to the config, TNDS service data will be added as well. Run the server in development mode with
+which will download NPTG, NaPTAN and NSPL data and commits them, as well as doing some modifications. If you've added TNDS FTP credentials to the config, TNDS service data will be added as well. Run the server locally in development mode with
 
-```
+```bash
 nxb run
 ```
+
+or run the Docker container.
 
 ## App keys
 
--- migrations/versions/1466e2297214_merged_08.py
+++ migrations/versions/1466e2297214_merged_08.py
@@ -1,9 +1,16 @@
 """
-Added active property to stops
+Squashed migrations for 0.8
 
-Revision ID: 2c4c58e49aa5
-Revises: fe9548df3474
-Create Date: 2019-03-29 07:40:58.924376
+- fk for region on journey pattern
+- Added active property to stops
+- modified constraints for operating and special periods
+- modified fts weights
+- added service pairs
+- fixed services without operators
+
+Revision ID: 1466e2297214
+Revises: 2bf4c5301c63
+Create Date: 2019-07-19 13:51:55.584119
 
 """
 from alembic import op
@@ -11,8 +18,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2c4c58e49aa5'
-down_revision = 'fe9548df3474'
+revision = '1466e2297214'
+down_revision = '74069f6388f3'
 branch_labels = None
 depends_on = None
 
@@ -30,6 +37,8 @@ stop_point = sa.table(
 
 
 def upgrade():
+    op.create_foreign_key(op.f('journey_pattern_region_ref_fkey'), 'journey_pattern', 'region', ['region_ref'], ['code'], ondelete='CASCADE')
+
     op.add_column('stop_area', sa.Column('active', sa.Boolean(), nullable=True))
     op.add_column('stop_point', sa.Column('active', sa.Boolean(), nullable=True))
     op.execute(sa.update(stop_area).values(active=True))
@@ -39,11 +48,36 @@ def upgrade():
     op.create_index(op.f('ix_stop_area_active'), 'stop_area', ['active'], unique=False)
     op.create_index(op.f('ix_stop_point_active'), 'stop_point', ['active'], unique=False)
 
+    op.drop_constraint('operating_period_check', 'operating_period')
+    op.drop_constraint('special_period_check', 'special_period')
+    op.alter_column('special_period', 'date_end', existing_type=sa.DATE(), nullable=False)
+    op.alter_column('special_period', 'date_start', existing_type=sa.DATE(), nullable=False)
+
+    op.create_table(
+        'service_pair',
+        sa.Column('id', sa.Integer(), autoincrement=False, nullable=False),
+        sa.Column('service0', sa.Integer(), nullable=False),
+        sa.Column('direction0', sa.Boolean(), nullable=False),
+        sa.Column('count0', sa.Integer(), nullable=False),
+        sa.Column('service1', sa.Integer(), nullable=False),
+        sa.Column('direction1', sa.Boolean(), nullable=False),
+        sa.Column('count1', sa.Integer(), nullable=False),
+        sa.Column('similarity', sa.Float(), nullable=False),
+        sa.CheckConstraint('service0 < service1'),
+        sa.ForeignKeyConstraint(('service0',), ['service.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(('service1',), ['service.id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_service_pair_direction0'), 'service_pair', ['direction0'], unique=False)
+    op.create_index(op.f('ix_service_pair_direction1'), 'service_pair', ['direction1'], unique=False)
+    op.create_index(op.f('ix_service_pair_service0'), 'service_pair', ['service0'], unique=False)
+    op.create_index(op.f('ix_service_pair_service1'), 'service_pair', ['service1'], unique=False)
+
     op.execute("DROP MATERIALIZED VIEW fts;")
     op.execute("""
         CREATE MATERIALIZED VIEW fts AS
         SELECT 'region' AS table_name,
-                region.code AS code,
+                CAST(region.code AS TEXT) AS code,
                 region.name AS name,
                 NULL AS short_ind,
                 NULL AS street,
@@ -59,7 +93,7 @@ def upgrade():
         WHERE region.code != 'GB'
         UNION ALL
         SELECT 'admin_area' AS table_name,
-               admin_area.code AS code,
+               CAST(admin_area.code AS TEXT) AS code,
                admin_area.name AS name,
                NULL AS short_ind,
                NULL AS street,
@@ -69,13 +103,13 @@ def upgrade():
                NULL AS district_name,
                admin_area.code AS admin_area_ref,
                admin_area.name AS admin_area_name,
-               ARRAY[admin_area.code] AS admin_areas,
+               CAST(ARRAY[admin_area.code] AS TEXT[]) AS admin_areas,
                setweight(to_tsvector('english', admin_area.name), 'A') AS vector
         FROM admin_area
         WHERE admin_area.region_ref != 'GB'
         UNION ALL
         SELECT 'district' AS table_name,
-               district.code AS code,
+               CAST(district.code AS TEXT) AS code,
                district.name AS name,
                NULL AS short_ind,
                NULL AS street,
@@ -85,14 +119,14 @@ def upgrade():
                NULL AS district_name,
                admin_area.code AS admin_area_ref,
                admin_area.name AS admin_area_name,
-               ARRAY[admin_area.code] AS admin_areas,
+               CAST(ARRAY[admin_area.code] AS TEXT[]) AS admin_areas,
                setweight(to_tsvector('english', district.name), 'A') ||
                setweight(to_tsvector('english', admin_area.name), 'B') AS vector
         FROM district
              JOIN admin_area ON admin_area.code = district.admin_area_ref
         UNION ALL
         SELECT 'locality' AS table_name,
-               locality.code AS code,
+               CAST(locality.code AS TEXT) AS code,
                locality.name AS name,
                NULL AS short_ind,
                NULL AS street,
@@ -102,10 +136,10 @@ def upgrade():
                district.name AS district_name,
                admin_area.code AS admin_area_ref,
                admin_area.name AS admin_area_name,
-               ARRAY[admin_area.code] AS admin_areas,
+               CAST(ARRAY[admin_area.code] AS TEXT[]) AS admin_areas,
                setweight(to_tsvector('english', locality.name), 'A') ||
-               setweight(to_tsvector('english', coalesce(district.name, '')), 'B') ||
-               setweight(to_tsvector('english', admin_area.name), 'B') AS vector
+               setweight(to_tsvector('english', coalesce(district.name, '')), 'C') ||
+               setweight(to_tsvector('english', admin_area.name), 'C') AS vector
         FROM locality
              LEFT OUTER JOIN district ON district.code = locality.district_ref
              JOIN admin_area ON admin_area.code = locality.admin_area_ref
@@ -116,7 +150,7 @@ def upgrade():
               )
         UNION ALL
         SELECT 'stop_area' AS table_name,
-               stop_area.code AS code,
+               CAST(stop_area.code AS TEXT) AS code,
                stop_area.name AS name,
                CAST(count(stop_point.atco_code) AS TEXT) AS short_ind,
                NULL AS street,
@@ -126,8 +160,8 @@ def upgrade():
                district.name AS district_name,
                admin_area.code AS admin_area_ref,
                admin_area.name AS admin_area_name,
-               ARRAY[admin_area.code] AS admin_areas,
-               setweight(to_tsvector('english', stop_area.name), 'A') ||
+               CAST(ARRAY[admin_area.code] AS TEXT[]) AS admin_areas,
+               setweight(to_tsvector('english', stop_area.name), 'B') ||
                setweight(to_tsvector('english', coalesce(locality.name, '')), 'C') ||
                setweight(to_tsvector('english', coalesce(district.name, '')), 'D') ||
                setweight(to_tsvector('english', admin_area.name), 'D') AS vector
@@ -140,7 +174,7 @@ def upgrade():
         GROUP BY stop_area.code, locality.name, district.name, admin_area.code
         UNION ALL
         SELECT 'stop_point' AS table_name,
-               stop_point.atco_code AS code,
+               CAST(stop_point.atco_code AS TEXT) AS code,
                stop_point.name AS name,
                stop_point.short_ind AS short_ind,
                stop_point.street AS street,
@@ -150,8 +184,8 @@ def upgrade():
                district.name AS district_name,
                admin_area.code AS admin_area_ref,
                admin_area.name AS admin_area_name,
-               ARRAY[admin_area.code] AS admin_areas,
-               setweight(to_tsvector('english', stop_point.name), 'A') ||
+               CAST(ARRAY[admin_area.code] AS TEXT[]) AS admin_areas,
+               setweight(to_tsvector('english', stop_point.name), 'B') ||
                setweight(to_tsvector('english', stop_point.street), 'B') ||
                setweight(to_tsvector('english', locality.name), 'C') ||
                setweight(to_tsvector('english', coalesce(district.name, '')), 'D') ||
@@ -173,10 +207,10 @@ def upgrade():
                NULL AS district_name,
                NULL AS admin_area_ref,
                NULL AS admin_area_name,
-               array_agg(DISTINCT admin_area.code) AS admin_areas,
-               setweight(to_tsvector('english', service.line), 'A') ||
-               setweight(to_tsvector('english', service.description), 'A') ||
-               setweight(to_tsvector('english', string_agg(DISTINCT operator.name, ' ')), 'B') ||
+               CAST(array_agg(DISTINCT admin_area.code) AS TEXT[]) AS admin_areas,
+               setweight(to_tsvector('english', service.line), 'B') ||
+               setweight(to_tsvector('english', service.description), 'B') ||
+               setweight(to_tsvector('english', coalesce(string_agg(DISTINCT operator.name, ' '), '')), 'C') ||
                setweight(to_tsvector('english', string_agg(DISTINCT locality.name, ' ')), 'C') ||
                setweight(to_tsvector('english', coalesce(string_agg(DISTINCT district.name, ' '), '')), 'D') ||
                setweight(to_tsvector('english', string_agg(DISTINCT admin_area.name, ' ')), 'D') AS vector
@@ -184,7 +218,7 @@ def upgrade():
              JOIN journey_pattern ON journey_pattern.service_ref = service.id
              JOIN local_operator ON local_operator.code = journey_pattern.local_operator_ref AND
                                     local_operator.region_ref = journey_pattern.region_ref
-             JOIN operator ON local_operator.operator_ref = operator.code
+             LEFT OUTER JOIN operator ON local_operator.operator_ref = operator.code
              JOIN journey_link ON journey_pattern.id = journey_link.pattern_ref
              JOIN stop_point ON journey_link.stop_point_ref = stop_point.atco_code AND stop_point.active
              JOIN locality ON stop_point.locality_ref = locality.code
@@ -201,10 +235,16 @@ def upgrade():
 
 
 def downgrade():
-    op.drop_index(op.f('ix_stop_point_active'), table_name='stop_point')
-    op.drop_index(op.f('ix_stop_area_active'), table_name='stop_point')
-    op.drop_column('stop_point', 'active')
-    op.drop_column('stop_area', 'active')
+    op.drop_index(op.f('ix_service_pair_service1'), table_name='service_pair')
+    op.drop_index(op.f('ix_service_pair_service0'), table_name='service_pair')
+    op.drop_index(op.f('ix_service_pair_direction1'), table_name='service_pair')
+    op.drop_index(op.f('ix_service_pair_direction0'), table_name='service_pair')
+    op.drop_table('service_pair')
+
+    op.alter_column('special_period', 'date_start', existing_type=sa.DATE(), nullable=True)
+    op.alter_column('special_period', 'date_end', existing_type=sa.DATE(), nullable=True)
+    op.create_check_constraint('operating_period_check', 'operating_period', sa.column('date_start') <= sa.column('date_end'))
+    op.create_check_constraint('special_period_check', 'special_period', sa.column('date_start') <= sa.column('date_end'))
 
     op.execute("DROP MATERIALIZED VIEW fts;")
     op.execute("""
@@ -363,3 +403,10 @@ def downgrade():
     op.create_index(op.f("ix_fts_unique"), "fts", ["table_name", "code"], unique=True)
     op.create_index(op.f("ix_fts_vector_gin"), "fts", ["vector"], unique=False, postgresql_using="gin")
     op.create_index(op.f("ix_fts_areas_gin"), "fts", ["admin_areas"], unique=False, postgresql_using="gin")
+
+    op.drop_index(op.f('ix_stop_point_active'), table_name='stop_point')
+    op.drop_index(op.f('ix_stop_area_active'), table_name='stop_point')
+    op.drop_column('stop_point', 'active')
+    op.drop_column('stop_area', 'active')
+
+    op.drop_constraint(op.f('journey_pattern_region_ref_fkey'), 'journey_pattern', type_='foreignkey')

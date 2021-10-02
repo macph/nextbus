146cdfc84d8c0dba125f173c989c6da8b4489841
macph <macph@users.noreply.github.com>
2021-10-02 17:10:35 +0100

    Merge last two migrations

--- migrations/versions/32c1e7049b33_nullable_columns.py
+++ migrations/versions/32c1e7049b33_nullable_columns.py
@@ -1,5 +1,10 @@
 """
-Nullable origin and destination for journey patterns
+Nullable columns
+
+Make the following columns nullable:
+- postcode.admin_area_ref
+- journey_pattern.origin
+- journey_pattern.destination
 
 Revision ID: 32c1e7049b33
 Revises: f543b9c57b76
@@ -12,12 +17,13 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '32c1e7049b33'
-down_revision = 'f543b9c57b76'
+down_revision = 'a435563da77e'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
+    op.alter_column('postcode', 'admin_area_ref', nullable=True)
     op.alter_column('journey_pattern', 'origin', nullable=True)
     op.alter_column('journey_pattern', 'destination', nullable=True)
 
@@ -25,3 +31,4 @@ def upgrade():
 def downgrade():
     op.alter_column('journey_pattern', 'destination', nullable=False)
     op.alter_column('journey_pattern', 'origin', nullable=False)
+    op.alter_column('postcode', 'admin_area_ref', nullable=False)
